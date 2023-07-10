0b2a4e6e599e2b330dbf8efe3cfa855f99350da2
Jeremy Watt <jermwatt@gmail.com>
2023-07-10 12:08:52 -0700

    refactoring boto controller

--- easy_boto3/__init__.py
+++ easy_boto3/__init__.py
@@ -1,19 +1,17 @@
 import os
 import sys
 from easy_boto3.utilities.session_maker import SessionAuthenticator
-from dotenv import dotenv_values
+from easy_boto3.utilities.aws_profile_parser import get_aws_metadata
 
-# add key directory to path
-sys.path.insert(0, '/Users/wattjer/.ssh')
+### load in aws metadata ###
+aws_metadata = get_aws_metadata()
 
+### add paths ###
 # path to this file
 file_path = os.path.abspath(__file__)
 
 # path to this file's directory
 parent_directory = '/'.join(file_path.split('/')[:-1])
-
-# path to parent of parent
-parent_of_parent_directory = '/'.join(file_path.split('/')[:-2])
 
 # path to base directory
 base_directory = os.path.dirname(os.path.dirname(os.path.dirname(file_path)))
@@ -22,16 +20,8 @@ base_directory = os.path.dirname(os.path.dirname(os.path.dirname(file_path)))
 sys.path.append(parent_directory)
 sys.path.append(base_directory)
 
-# load in environment variables
-config = dotenv_values(parent_of_parent_directory + "/.env")
 
-# instantiate progress logger
-session_auth = SessionAuthenticator(profile_name=config['profile_name'],
-                                    region_name=config['region_name'])
-
-# call out ids
-aws_access_key_id = config['aws_access_key_id']
-aws_secret_access_key = config['aws_secret_access_key']
-
-# define path to scripts directory
-scripts_path = os.path.join(parent_directory, 'scripts')
+### instance of decorators ###
+session_auth = SessionAuthenticator(
+    profile_name=aws_metadata['aws_profile_name'],
+    region_name=aws_metadata['aws_profile_region'])
--- easy_boto3/main.py
+++ easy_boto3/main.py
@@ -1,5 +1,5 @@
 import os
-import yaml 
+from easy_boto3 import aws_metadata, session_auth
 
 # path to this file and parent directory
 file_path = os.path.abspath(__file__)
@@ -7,17 +7,4 @@ parent_directory = '/'.join(file_path.split('/')[:-1])
 
  
 def main():
-    with open(parent_directory + '/.easy_boto3.yaml') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    print(config)
-    
-    
-    # import log path
-    with open(config, "r") as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    log_path = config["data"]["log_path"]
-
-
-
-if __name__ == '__main__':
-    
+    pass
