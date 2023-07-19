667f8eca6de1f52cbfa08edb4ceec3230f20f43b
Jeremy Watt <jermwatt@gmail.com>
2023-07-19 15:12:59 -0700

    Update __init__.py

--- easy_boto3/__init__.py
+++ easy_boto3/__init__.py
@@ -15,4 +15,11 @@ internal_config_path = library_path + '/.easy_boto3_internal.yaml'
 
 # if internal yaml file does not exist, create it
 if not Path(internal_config_path).exists():
+    # create internal file
     Path(internal_config_path).touch()
+
+    # add aws_profile: default to internal yaml file
+    with open(internal_config_path, 'w') as f:
+        f.write('aws_profile: default')
+
+
